6b16c365095c90da9cbba3bfb2950980b116def3
Masibo <victor.wafula@student.moringaschool.com>
2023-09-15 00:16:46 +0300

    Completed Assignment

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,43 @@
+# Function to create an empty list
 def create_an_empty_list():
-    return None
+    return []
 
+# Function to create a list with four elements
 def create_a_list():
-    return None
+    return [1, 5, 8, 5]
 
-def add_element_to_end_of_list(l, element):
-    return None
+# Function to add an element to the end of a list and return the updated list
+def add_element_to_end_of_list(my_list, element):
+    my_list.append(element)
+    return my_list
 
-def add_element_to_start_of_list(l, element):
-    return None
+# Function to add an element to the start of a list
+def add_element_to_start_of_list(my_list, element):
+    my_list.insert(0, element)
 
-def remove_element_from_end_of_list(l):
-    return None
+# Function to remove the last element from a list
+def remove_element_from_end_of_list(my_list):
+    my_list.pop()
 
-def remove_element_from_start_of_list(l):
-    return None
+# Function to remove the first element from a list
+def remove_element_from_start_of_list(my_list):
+    del my_list[0]
 
-def retrieve_first_element_from_list(l):
-    return None
+# Function to retrieve the first element from a list
+def retrieve_first_element_from_list(my_list):
+    return my_list[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+# Function to retrieve an element from a specific index in a list
+def retrieve_element_from_index(my_list, index):
+    if index < len(my_list):
+        return my_list[index]
+    else:
+        return None  # Return None if the index is out of bounds
 
-def retrieve_last_element_from_list(l):
-    return None
+# Function to retrieve the last element from a list
+def retrieve_last_element_from_list(my_list):
+    if len(my_list) > 0:
+        return my_list[-1]
+    else:
+        return None  # Return None if the list is empty
+
